afedfdeb0745d3bbfad94f5cc457cf644973064f
Lior Rabin <lior@fuse.io>
2019-12-22 16:47:13 +0200

    invite api

--- lib/src/api.dart
+++ lib/src/api.dart
@@ -194,4 +194,9 @@ class API {
     Map<String, dynamic> resp = await _post('v2/contacts/$nonce', private: true);
     return resp;
   }
+
+  Future<dynamic> invite(String phoneNumber, String communityAddress) async {
+    Map<String, dynamic> resp = await _post('v2/wallets/invite/$phoneNumber', body: {"communityAddress": communityAddress}, private: true);
+    return resp;
+  }
 }
