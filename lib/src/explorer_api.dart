3196b4268753699ca6f89a5870c3880eb435cac8
Lior Agnin <Lior.Agnin@gmail.com>
2020-08-10 15:05:13 +0300

    changed token_api to explorer api (#52)

--- lib/src/explorer_api.dart
+++ lib/src/explorer_api.dart
@@ -1,37 +1,29 @@
-library tokens_api;
+library explorer_api;
 
 import 'dart:async';
 
 import 'package:http/http.dart';
 import 'package:wallet_core/models/api.dart';
 
-const String ETHEREUM_API_BASE_URL = 'https://api.etherscan.io/api';
-
-class TokensApi extends Api {
+class ExplorerApi extends Api {
   String _base;
   String _apiKey;
   Client _client;
 
-  TokensApi({String etherscanBase, String etherscanApiKey}) {
-    _base = etherscanBase ?? ETHEREUM_API_BASE_URL;
-    _apiKey = etherscanApiKey;
+  ExplorerApi({String base, String apiKey}) {
+    _base = base;
+    _apiKey = apiKey;
     _client = new Client();
   }
 
   Future<Map<String, dynamic>> _get(String endpoint) async {
-    Response response = await _client.get('$_base/$endpoint');
-    return responseHandler(response);
-  }
-
-  Future<dynamic> getTransferEventsByAccountAddress(String accountAddress,
-      {int startblock = 0, endBlock = 999999999, String sort = 'desc'}) async {
-    try {
-      Map<String, dynamic> resp = await _get(
-          '?module=account&action=tokentx&address=$accountAddress&startblock=$startblock&endblock=$endBlock&sort=$sort&apikey=$_apiKey');
-      return resp['result'];
-    } catch (e) {
-      throw e;
+    Response response;
+    if ([null, ''].contains(_apiKey)) {
+      response = await _client.get('$_base$endpoint');
+    } else {
+      response = await _client.get('$_base$endpoint&apikey=$_apiKey');
     }
+    return responseHandler(response);
   }
 
   Future<List<dynamic>> getTokenTransferEventsByAccountAddress(
@@ -39,7 +31,7 @@ class TokensApi extends Api {
       {String sort = 'desc', int startblock = 0}) async {
     try {
       Map<String, dynamic> resp = await _get(
-          '?module=account&action=tokentx&contractaddress=$tokenAddress&address=$accountAddress&startblock=$startblock&sort=$sort&apikey=$_apiKey');
+          '?module=account&action=tokentx&contractaddress=$tokenAddress&address=$accountAddress&startblock=$startblock&sort=$sort');
       if (resp['message'] == 'OK' && resp['status'] == '1') {
         List transfers = [];
         for (dynamic transferEvent in resp['result']) {
@@ -50,7 +42,7 @@ class TokensApi extends Api {
             'from': transferEvent["from"],
             'status': "CONFIRMED",
             'timestamp': int.parse(transferEvent['timeStamp'].toString()),
-            'value': BigInt.from(num.parse(transferEvent['value'])),
+            'value': BigInt.parse(transferEvent['value']),
             'tokenAddress': tokenAddress,
             'type': transferEvent["from"].toString().toLowerCase() ==
                     accountAddress.toLowerCase()
@@ -63,19 +55,20 @@ class TokensApi extends Api {
         return [];
       }
     } catch (e) {
-      throw e;
+      throw 'Error! Get token transfers events failed for - accountAddress: $accountAddress --- $e';
     }
   }
 
   Future<BigInt> getTokenBalanceByAccountAddress(
-      String tokenAddress, String accountAddress,
-      {int startblock = 0, endBlock = 999999999}) async {
+    String tokenAddress,
+    String accountAddress,
+  ) async {
     try {
       Map<String, dynamic> resp = await _get(
-          '?module=account&action=tokenbalance&contractaddress=$tokenAddress&address=$accountAddress&tag=latest&apikey=$_apiKey');
+          '?module=account&action=tokenbalance&contractaddress=$tokenAddress&address=$accountAddress');
       return BigInt.from(num.parse(resp['result']));
     } catch (e) {
-      throw e;
+      throw 'Error! Get token balance failed for - accountAddress: $accountAddress --- $e';
     }
   }
 }
