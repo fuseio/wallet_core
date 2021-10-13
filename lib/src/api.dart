81dcd95f9a06fc59a6d75cd070f4823ad882216e
artman-industries <artman.industries2020@gmail.com>
2021-10-13 16:11:55 +0300

    get beacon by wallet address

--- lib/src/api.dart
+++ lib/src/api.dart
@@ -781,4 +781,14 @@ class API extends Api {
     );
     return resp;
   }
+    
+
+    Future<Map<String, dynamic>> getBeaconByWalletAddress(String walletAddress) async {
+    String url = 'v2/wallets/beacons/$walletAddress';
+    Map<String, dynamic> resp = await _post(
+      url,
+      private: true,
+    );
+    return resp['data'];
+  }
 }
