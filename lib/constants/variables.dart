a06b96069a9dd35457cb614c46bddc49f6a88b0b
Lior Agnin <lior.agnin@gmail.com>
2021-11-14 19:25:03 +0200

    improve error handling

--- lib/constants/variables.dart
+++ lib/constants/variables.dart
@@ -2,5 +2,6 @@ class Variables {
   Variables._();
 
   static const int DEFAULT_GAS_LIMIT = 700000;
-  static const String NATIVE_TOKEN_ADDRESS = '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'; // For sending native (ETH/FUSE) using TransferManager
+  static const String NATIVE_TOKEN_ADDRESS =
+      '0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE'; // For sending native (ETH/FUSE) using TransferManager
 }
