1625a79b46421e76d1b9f60effbf0eaf12ea7830
Lior Agnin <Lior.Agnin@gmail.com>
2022-04-17 10:55:24 +0300

    minor fix (#95)

--- lib/src/studio_api.dart
+++ lib/src/studio_api.dart
@@ -2,7 +2,7 @@ library api;
 
 import 'dart:async';
 import 'dart:io';
-import 'package:path/path.dart';
+import 'package:path/path.dart' show basename;
 
 import 'package:dio/dio.dart';
 import 'package:pretty_dio_logger/pretty_dio_logger.dart';
